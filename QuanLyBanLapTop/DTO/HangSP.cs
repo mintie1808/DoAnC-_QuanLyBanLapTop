87abfe16cd982528c53c88d2a9eb933f07a89154
tronghk <88172531+tronghk@users.noreply.github.com>
2022-09-20 14:02:35 +0700

    Trong- Đối tượng - Xong

---
+++ QuanLyBanLapTop/DTO/HangSP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HangSP
+    {
+        private int id;
+        private string nameManufacturer;
+
+        public int Id   // property
+        {
+            get { return id; }   // get method
+            set { id = value; }  // set method
+        }
+
+        public string NameManufacturer   // property
+        {
+            get { return nameManufacturer; }   // get method
+            set { nameManufacturer = value; }  // set method
+        }
+
+    }
+}
