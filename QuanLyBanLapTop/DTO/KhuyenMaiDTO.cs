1d6b58d09335382fd7b501828ec5640810481e4b
mintie1808 <mintie1808@gmail.com>
2022-09-20 15:52:17 +0700

    e

--- QuanLyBanLapTop/DTO/KhuyenMaiDTO.cs
+++ QuanLyBanLapTop/DTO/KhuyenMaiDTO.cs
@@ -8,5 +8,43 @@ namespace DTO
 {
     public class KhuyenMaiDTO
     {
+        private int id;
+        private string namePromotion;
+        private string status;
+        private DateTime startDay;
+        private DateTime endDay;
+        private int idTypeProduct;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string NamePromotion
+        {
+            get { return namePromotion; }
+            set { namePromotion = value; }
+        }
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public DateTime StartDay
+        {
+            get { return startDay; }
+            set { startDay = value; }
+        }
+        public DateTime EndDay
+        {
+            get { return endDay; }
+            set { endDay = value; }
+        }
+        public int IdTypeProduct
+        {
+            get { return idTypeProduct; }
+            set { idTypeProduct = value; }
+        }
     }
 }
