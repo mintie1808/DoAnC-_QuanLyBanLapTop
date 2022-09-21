9a434d8055588e5d5454c7b21a41a32a5ae5c976
Nguyen Hoang Quan <107245089+BlackIcon058@users.noreply.github.com>
2022-09-21 21:17:17 +0700

    Add files via upload

---
+++ QuanLyBanLapTop/DTO/HoaDon.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DTO
+{
+    public class HoaDon
+    {
+        private int id;
+        private DateTime dateBill;
+        private float sumPay;
+        private string typePay;
+        private float discountMoney;
+        private float pay;
+        private string status;
+
+
+        public int idEmployee
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public DateTime dateBill
+        {
+            get { return dateBill; }
+            set { dateBill = value; }
+        }
+
+        public float sumPay
+        {
+            get { return sumPay; }
+            set { sumPay = value; }
+        }
+
+        public string typePay
+        {
+            get { return typePay; }
+            set { typePay = value; }
+        }
+
+        public float discountMoney
+        {
+            get { return discountMoney; }
+            set { discountMoney = value; }
+        }
+
+        public float pay
+        {
+            get { return pay; }
+            set { pay = value; }
+        }
+
+        public string status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+    }
+
+}
