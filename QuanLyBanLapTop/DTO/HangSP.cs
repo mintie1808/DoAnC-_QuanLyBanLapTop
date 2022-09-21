0b3eb7d1cafb6c15b8c484f276af40c6ce81c3c6
Kudo69 <86213330+Kudo69@users.noreply.github.com>
2022-09-21 15:15:23 +0700

    Đối tượng DTO - Anh - Đã xong
    Đối tượng DTO - Anh - Đã xong

--- QuanLyBanLapTop/DTO/HangSP.cs
+++ QuanLyBanLapTop/DTO/HangSP.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,22 +6,19 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    public class HangSP
+    public int id;
+    public string nameManufacturer;
+
+    public int ID
     {
-        private int id;
-        private string nameManufacturer;
-
-        public int Id   // property
-        {
-            get { return id; }   // get method
-            set { id = value; }  // set method
-        }
-
-        public string NameManufacturer   // property
-        {
-            get { return nameManufacturer; }   // get method
-            set { nameManufacturer = value; }  // set method
-        }
-
+        get { return id; }
+        set { id = value;}
     }
+
+    public string nameManufacturer
+    {
+        get { return nameManufacturer; }
+        set { nameManufacturer = value; }
+    }
+
 }
