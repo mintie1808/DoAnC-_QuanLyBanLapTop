cab6fbf4cc6de701632ec67e038b77f78b51ac4a
ThanhTrucOwO <60774033+ThanhTrucOwO@users.noreply.github.com>
2022-09-25 17:06:05 +0700

    Update sqlprocPhieuNhapKho.sql

--- Proc/sqlprocPhieuNhapKho.sql
+++ Proc/sqlprocPhieuNhapKho.sql
@@ -3,15 +3,16 @@ GO
 
 CREATE PROC Proc_addReceipt
 /* tạo biến trùng tên với biến trong code */
-@idReceipt int,
-@idSupplier NVARCHAR(50),
-@dateReceipt DATE,
-@sumPay FLOAT
+@id int,
+@idSupplier NVARCHAR(50)
+
+/* @dateReceipt DATE,
+@sumPay FLOAT */
 as
 BEGIN 
 /* câu lệnh SELECT */
-INSERT INTO dbo.Tbl_PhieuNhapKho (idReceipt, idSupplier, dateReceipt, sumPay)
-VALUES (@idReceipt, @idSupplier, @dateReceipt, @sumPay)
+INSERT INTO dbo.Tbl_PhieuNhapKho (id, idSupplier)
+VALUES (@id, @idSupplier)
 END 
 
 CREATE PROC Proc_deleteReceipt
@@ -19,19 +20,17 @@ CREATE PROC Proc_deleteReceipt
 @idReceipt int
 as
 BEGIN 
-DELETE FROM dbo.Tbl_PhieuNhapKho WHERE idReceipt = @idReceipt
+DELETE FROM dbo.Tbl_PhieuNhapKho WHERE id = @id
 END 
 
 
 CREATE PROC Proc_updateReceipt
 /* tạo biến trùng tên với biến trong code */
 @idReceipt int,
-@idSupplier NVARCHAR(50),
-@dateReceipt DATE,
-@sumPay FLOAT
+@idSupplier NVARCHAR(50)
 as
 BEGIN 
 UPDATE dbo.Tbl_PhieuNhapKho
-SET idReceipt = @idReceipt, idSupplier = @idSupplier, dateReceipt = @dateReceipt, sumPay = @sumPay
-WHERE idReceipt = @idReceipt
+SET id = @id, idSupplier = @idSupplier
+WHERE id = @id
 END
