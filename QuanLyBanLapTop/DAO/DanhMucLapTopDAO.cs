1f23e22f3aed308f8290fe7282b12a0440f45846
Kudo69 <86213330+Kudo69@users.noreply.github.com>
2022-09-25 14:49:12 +0700

    Add files via upload

---
+++ QuanLyBanLapTop/DAO/DanhMucLapTopDAO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAO
+{
+    public class DanhMucLapTopDAO
+    {
+        private Connect conn = new Connect();
+
+        // them 1 danh muc
+        public void addDanhMucLapTop(DanhMucLapTop dm)
+        {
+            // ket noi voi connect
+            conn.Conn.Open();
+            // tao lop ket noi sqlCommand
+            SqlCommand command = new SqlCommand("Proc_addTypeProduct", conn.Conn);
+            // dat ten bien
+            command.CommandType = System.Data.CommandType.StoredProcedure; 
+            command.Parameters.AddWithValue("@id", dm.ID);
+            command.Parameters.AddWithValue("@name", dm.NameTypeProduct);
+            command.Parameters.AddWithValue("@idManufacturer", dm.IdManufacturer);
+            command.Connection = conn.Conn;
+            // tao bien reader
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+
+        public void updateDanhMucLapTop(DanhMucLapTop dm)
+        {
+            // ket noi voi connect
+            conn.Conn.Open();
+            // tao lop ket noi sqlCommand
+            SqlCommand command = new SqlCommand("Proc_updateTypeProduct", conn.Conn);
+            // dat ten bien
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@id", dm.ID);
+            command.Parameters.AddWithValue("@name", dm.NameTypeProduct);
+            command.Parameters.AddWithValue("@idManufacturer", dm.IdManufacturer);
+            command.Connection = conn.Conn;
+            // tao bien reader
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+
+        public void deleteDanhMucLapTop(int id)
+        {
+            // ket noi voi connect
+            conn.Conn.Open();
+            // tao lop ket noi sqlCommand
+            SqlCommand command = new SqlCommand("Proc_deleteTypeProduct", conn.Conn);
+            // dat ten bien
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@id", id);
+            command.Connection = conn.Conn;
+            // tao bien reader
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+
+    }
+}
---
+++ QuanLyBanLapTop/DAO/HangSPDAO.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAO
+{
+    public class HangSPDAO
+    {
+        private Connect conn = new Connect();
+
+        public void addHangSanPham(HangSP hsp)
+        {
+            // ket noi voi connect
+            conn.Conn.Open();
+            // tao lop ket noi sqlCommand
+            SqlCommand command = new SqlCommand("Proc_addManufacture", conn.Conn);
+            // dat ten bien
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@id", hsp.Id);
+            command.Parameters.AddWithValue("@name", hsp.NameManufacturer);
+            command.Connection = conn.Conn;
+            // tao bien reader
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+
+        public void updateHangSanPham(HangSP hsp)
+        {
+            // ket noi voi connect
+            conn.Conn.Open();
+            // tao lop ket noi sqlCommand
+            SqlCommand command = new SqlCommand("Proc_updateManufacture", conn.Conn);
+            // dat ten bien
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@id", hsp.Id);
+            command.Parameters.AddWithValue("@name", hsp.NameManufacturer);
+            command.Connection = conn.Conn;
+            // tao bien reader
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+
+        public void deleteHangSanPham(int id)
+        {
+            // ket noi voi connect
+            conn.Conn.Open();
+            // tao lop ket noi sqlCommand
+            SqlCommand command = new SqlCommand("Proc_deleteManufacture", conn.Conn);
+            // dat ten bien
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@id", id);
+            command.Connection = conn.Conn;
+            // tao bien reader
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+    }
+}
