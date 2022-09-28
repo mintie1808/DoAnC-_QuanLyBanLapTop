5123733170ec9a0037e8d42cf22d68fb02e7f941
mintie1808 <111961232+mintie1808@users.noreply.github.com>
2022-09-28 23:08:50 +0700

    Add files via upload

---
+++ QuanLyBanLapTop/DAO/KhuyenMaiDAO.cs
@@ -0,0 +1,72 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+        public class KhuyenMaiDAO
+    {
+        private Connect conn = new Connect();
+        
+        public void addKhuyenMai(KhuyenMaiDTO khuyenMai)
+        {
+          
+            conn.Conn.Open();
+           
+            SqlCommand command = new SqlCommand("Proc_addKhuyenMai", conn.Conn);
+            
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@idPromotion", khuyenMai.IdPromotion);
+            command.Parameters.AddWithValue("@namePromotion", khuyenMai.NamePromotion);
+            command.Parameters.AddWithValue("@status", khuyenMai.Status);
+            command.Parameters.AddWithValue("@startDay", khuyenMai.StartDay);
+            command.Parameters.AddWithValue("@endDay", khuyenMai.EndDay);
+            command.Connection = conn.Conn;
+          
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+        public void updateKhuyenMai(KhuyenMaiDTO khuyenMai)
+        {
+
+            conn.Conn.Open();
+
+            SqlCommand command = new SqlCommand("Proc_updateKhuyenMai", conn.Conn);
+
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@idPromotion", khuyenMai.IdPromotion);
+            command.Parameters.AddWithValue("@namePromotion", khuyenMai.NamePromotion);
+            command.Parameters.AddWithValue("@status", khuyenMai.Status);
+            command.Parameters.AddWithValue("@startDay", khuyenMai.StartDay);
+            command.Parameters.AddWithValue("@endDay", khuyenMai.EndDay);
+            command.Connection = conn.Conn;
+
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+        public void deleteKhuyenMai(int idPromotion)
+        {
+
+            conn.Conn.Open();
+
+            SqlCommand command = new SqlCommand("Proc_deleteKhuyenMai", conn.Conn);
+
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@idPromotion", idPromotion);
+           
+            command.Connection = conn.Conn;
+
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            conn.Conn.Close();
+        }
+
+    }
+}
